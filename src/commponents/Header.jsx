af503a1314c5bb15a316e78a777719641a21f4b3
youmna <youmnaelsaeed43@gmail.com>
2024-11-26 20:29:48 +0300

    last

--- src/commponents/Header.jsx
+++ src/commponents/Header.jsx
@@ -1,49 +1,60 @@
-import React, { useState } from "react";
-import { brainwave } from "../assets";
-import { navigation } from "../constants/index";
 import { useLocation } from "react-router-dom";
-import {disablePageScroll , enablPageScroll} from 'scroll-lock'
+import { disablePageScroll, enablePageScroll } from "scroll-lock";
+
+import { brainwave } from "../assets";
+import { navigation } from "../constants";
 import Button from "./Button";
 import MenuSvg from "../assets/svg/MenuSvg";
 import { HambugerMenu } from "./design/Header";
+import { useState } from "react";
 
 const Header = () => {
-  const currentPath = useLocation();
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const pathname = useLocation();
+  const [openNavigation, setOpenNavigation] = useState(false);
 
-  const handleMenuToggle = () => {
-    setIsMenuOpen((prevState) => !prevState);
-    if(isMenuOpen){
-      enablPageScroll()
-    }else{
-      disablePageScroll()
+  const toggleNavigation = () => {
+    if (openNavigation) {
+      setOpenNavigation(false);
+      enablePageScroll();
+    } else {
+      setOpenNavigation(true);
+      disablePageScroll();
     }
   };
 
-  const closeMenu = () => {
-    if(! isMenuOpen) return;
-    enablPageScroll()
-    setIsMenuOpen(false);
+  const handleClick = () => {
+    if (!openNavigation) return;
+
+    enablePageScroll();
+    setOpenNavigation(false);
   };
 
   return (
-    <header className={`fixed top-0 left-0 w-full z-50 border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${isMenuOpen ? 
-      "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
-      }`} >
+    <div
+      className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+        openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
+      }`}
+    >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
           <img src={brainwave} width={190} height={40} alt="Brainwave" />
         </a>
 
-        <nav className={`${ isMenuOpen ? "flex" : "hidden"} 
-        fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}>
+        <nav
+          className={`${
+            openNavigation ? "flex" : "hidden"
+          } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
+        >
           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
             {navigation.map((item) => (
-              <a  key={item.id} href={item.url} onClick={closeMenu}
-               className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
+              <a
+                key={item.id}
+                href={item.url}
+                onClick={handleClick}
+                className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
                   item.onlyMobile ? "lg:hidden" : ""
                 } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
-                  item.url === currentPath.hash
+                  item.url === pathname.hash
                     ? "z-2 lg:text-n-1"
                     : "lg:text-n-1/50"
                 } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
@@ -56,18 +67,25 @@ const Header = () => {
           <HambugerMenu />
         </nav>
 
-        <a href="#signup" className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block" >
+        <a
+          href="#signup"
+          className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
+        >
           New account
         </a>
         <Button className="hidden lg:flex" href="#login">
           Sign in
         </Button>
 
-        <Button className="ml-auto lg:hidden" px="px-3" onClick={handleMenuToggle}>
-          <MenuSvg openNavigation={isMenuOpen} />
+        <Button
+          className="ml-auto lg:hidden"
+          px="px-3"
+          onClick={toggleNavigation}
+        >
+          <MenuSvg openNavigation={openNavigation} />
         </Button>
       </div>
-    </header>
+    </div>
   );
 };
 
